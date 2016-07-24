abd6ea914c88c4fd7f67ed915f8743ec031bc3d6
Robert Everland III <reverland@gmail.com>
2016-07-23 23:12:29 -0400

    Checking in Elliot changes from last project.

--- musicStoreJPA/src/java/music/business/Product.java
+++ musicStoreJPA/src/java/music/business/Product.java
@@ -1,78 +1,84 @@
-package music.business;
 
-import java.text.NumberFormat;
+package music.business;
+/**
+ *
+ * @author Elliot Martinez
+ * @author Matthew Kastro
+ * 
+ * changelog: 7/13/16
+ *      Added JPA entity information
+ */
+
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.text.NumberFormat;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Product implements Serializable {
-
-    @Id
+    
+    @Id // primary key
+    @Column(name = "PRODUCTID", nullable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long productId;    
+    private Long productId;
+    
+    @Column(name = "CODE", nullable = false)
     private String code;
+    
+    @Column(name = "DESCRIPTION", nullable = false)
     private String description;
+    
+    @Column(name = "PRICE", nullable = false)
     private double price;
-
-    public Product() {}
-
+    
     public Long getId() {
         return productId;
     }
-
+    
     public void setId(Long productId) {
         this.productId = productId;
     }
-
+    
     public void setCode(String code) {
         this.code = code;
     }
-
+    
     public String getCode() {
         return code;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
-    public String getArtistName() {
-        String artistName = 
-                description.substring(0, description.indexOf(" - "));
-        return artistName;
-    }
-
-    public String getAlbumName() {
-        String albumName = 
-                description.substring(description.indexOf(" - ") + 3);
-        return albumName;
-    }
-
+    
     public void setPrice(double price) {
         this.price = price;
     }
-
+    
     public double getPrice() {
         return price;
     }
-
+    
+    public String getArtistName() {
+        String artistName = description.substring(0,description.indexOf(" - "));
+        return artistName;
+    }
+    
+    public String getAlbumName() {
+        String albumName = 
+                description.substring(0, description.indexOf(" - ")+3);
+        return albumName;
+    }
+    
     public String getPriceCurrencyFormat() {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         return currency.format(price);
     }
-
-    public String getImageURL() {
-        String imageURL = "/musicStore/images/" + code + "_cover.jpg";
-        return imageURL;
-    }
-
+    
     public String getProductType() {
         return "Audio CD";
     }
